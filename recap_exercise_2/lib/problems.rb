0bc3d67decbcb9bb16281f3c9082909c43e9c94e
Jacob Tabor <jtabor214@gmail.com>
2023-08-31 17:26:12 -0400

    completed least_common_multiple

--- recap_exercise_2/lib/problems.rb
+++ recap_exercise_2/lib/problems.rb
@@ -1,7 +1,9 @@
 # Write a method, least_common_multiple, that takes in two numbers and returns
 # the smallest number that is a mutiple of both of the given numbers
 def least_common_multiple(num_1, num_2)
-  
+    
+  num_1.lcm(num_2)  
+
 end
 
 
