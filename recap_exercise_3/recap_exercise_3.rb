6a8cfc246de54a761cf0137e9b10e6d71d4339ce
Jacob Tabor <jtabor214@gmail.com>
2023-09-01 00:18:07 -0400

    finished char_indices

--- recap_exercise_3/recap_exercise_3.rb
+++ recap_exercise_3/recap_exercise_3.rb
@@ -27,21 +27,38 @@
 
 # Write a method `no_consecutive_repeats?(arr)` that accepts an array as an arg.
 # The method should return `true` if an element never appears consecutively in the
-# array; it should return `false` otherwise.
+# # array; it should return `false` otherwise.
 
-def no_consecutive_repeats?(arr)
+# def no_consecutive_repeats?(arr)
 
-    arr.each_with_index do |ele, idx|
-        if ele == arr[idx + 1]
-            return false
-        end
-    end
-    return true
+#     arr.each_with_index do |ele, idx|
+#         if ele == arr[idx + 1]
+#             return false
+#         end
+#     end
+#     return true
+# end
+
+
+# p no_consecutive_repeats?(['cat', 'dog', 'mouse', 'dog'])     # => true
+# p no_consecutive_repeats?(['cat', 'dog', 'dog', 'mouse'])     # => false
+# p no_consecutive_repeats?([10, 42, 3, 7, 10, 3])              # => true
+# p no_consecutive_repeats?([10, 42, 3, 3, 10, 3])              # => false
+# p no_consecutive_repeats?(['x'])                              # => true
+
+# Write a method `char_indices(str)` that takes in a string as an arg. The method
+# should return a hash containing characters as keys. The value associated with
+# each key should be an array containing the indices where that character is
+# found.
+
+def char_indices(str)
+    hash = Hash.new {|h, k| h[k] = [] }
+
+    str.each_char.with_index do |char, idx|
+        hash[char] << idx
+    end 
+    hash
 end
 
-
-p no_consecutive_repeats?(['cat', 'dog', 'mouse', 'dog'])     # => true
-p no_consecutive_repeats?(['cat', 'dog', 'dog', 'mouse'])     # => false
-p no_consecutive_repeats?([10, 42, 3, 7, 10, 3])              # => true
-p no_consecutive_repeats?([10, 42, 3, 3, 10, 3])              # => false
-p no_consecutive_repeats?(['x'])                              # => true
+p char_indices('mississippi')   # => {"m"=>[0], "i"=>[1, 4, 7, 10], "s"=>[2, 3, 5, 6], "p"=>[8, 9]}
+p char_indices('classroom')     # => {"c"=>[0], "l"=>[1], "a"=>[2], "s"=>[3, 4], "r"=>[5], "o"=>[6, 7], "m"=>[8]}
