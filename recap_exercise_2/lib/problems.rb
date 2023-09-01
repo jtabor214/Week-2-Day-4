49800b0222147d4fd35ad956fcf73746d4ef7961
Jacob Tabor <jtabor214@gmail.com>
2023-08-31 23:48:18 -0400

    finished bubble_sort

--- recap_exercise_2/lib/problems.rb
+++ recap_exercise_2/lib/problems.rb
@@ -64,5 +64,21 @@ class Array
   # This should remind you of the spaceship operator! Convenient :)
   def bubble_sort(&prc)
     
+    prc ||= Proc.new { |k, v| k <=> v }
+
+      
+    sorted = false
+      while !sorted
+        sorted = true
+
+        (0...self.length - 1).each do |i|
+          if prc.call(self[i], self[i + 1 ]) == 1 # == 1 here insures that the array will sort itself in increasing order
+            self[i], self[i + 1] = self[i + 1], self[i]
+              original = sorted 
+              sorted = false
+          end
+        end
+      end
+    self
   end
 end
