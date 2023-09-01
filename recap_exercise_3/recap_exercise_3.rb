f7dfd43556d3069c1cde083551a29a8b27c21720
Jacob Tabor <jtabor214@gmail.com>
2023-09-01 00:39:00 -0400

    finished longest_streak

--- recap_exercise_3/recap_exercise_3.rb
+++ recap_exercise_3/recap_exercise_3.rb
@@ -51,14 +51,45 @@
 # each key should be an array containing the indices where that character is
 # found.
 
-def char_indices(str)
-    hash = Hash.new {|h, k| h[k] = [] }
+# def char_indices(str)
+#     hash = Hash.new {|h, k| h[k] = [] }
 
-    str.each_char.with_index do |char, idx|
-        hash[char] << idx
-    end 
-    hash
+#     str.each_char.with_index do |char, idx|
+#         hash[char] << idx
+#     end 
+#     hash
+# end
+
+# p char_indices('mississippi')   # => {"m"=>[0], "i"=>[1, 4, 7, 10], "s"=>[2, 3, 5, 6], "p"=>[8, 9]}
+# p char_indices('classroom')     # => {"c"=>[0], "l"=>[1], "a"=>[2], "s"=>[3, 4], "r"=>[5], "o"=>[6, 7], "m"=>[8]}
+
+
+# Write a method `longest_streak(str)` that accepts a string as an arg. The method
+# should return the longest streak of consecutive characters in the string. If
+# there are any ties, return the streak that occurs later in the string.
+
+def longest_streak(str)
+    current = ''
+    longest = ''
+
+    (0...str.length).each do |idx|
+        if str[idx] == str[idx - 1]
+            current += str[idx]
+        else 
+            current = str[idx]
+        end
+
+        if current.length >= longest.length
+            longest = current
+        end
+    end
+    longest
+    
 end
 
-p char_indices('mississippi')   # => {"m"=>[0], "i"=>[1, 4, 7, 10], "s"=>[2, 3, 5, 6], "p"=>[8, 9]}
-p char_indices('classroom')     # => {"c"=>[0], "l"=>[1], "a"=>[2], "s"=>[3, 4], "r"=>[5], "o"=>[6, 7], "m"=>[8]}
+
+p longest_streak('a')           # => 'a'
+p longest_streak('accccbbb')    # => 'cccc'
+p longest_streak('aaaxyyyyyzz') # => 'yyyyy
+p longest_streak('aaabbb')      # => 'bbb'
+p longest_streak('abc')         # => 'c'
